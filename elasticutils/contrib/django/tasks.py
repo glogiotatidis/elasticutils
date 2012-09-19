c47084d13191e4c3d67d776e871b55296965ea70
Giorgos Logiotatidis <seadog@sealabs.net>
2012-09-19 11:56:14 +0300

    Update index and unindex tasks to new API.

--- elasticutils/contrib/django/tasks.py
+++ elasticutils/contrib/django/tasks.py
@@ -13,7 +13,7 @@ log = logging.getLogger('elasticutils')
 def index_objects(model, ids, **kw):
     """Models can asynchronously update their ES index.
 
-    If a model extends SearchMixin, it can add a post_save hook like so::
+    If a model extends Indexable, it can add a post_save hook like so::
 
         @receiver(dbsignals.post_save, sender=MyModel)
         def update_search_index(sender, instance, **kw):
@@ -23,13 +23,13 @@ def index_objects(model, ids, **kw):
     """
     if settings.ES_DISABLED:
         return
+
     es = get_es()
     log.info('Indexing objects %s-%s. [%s]' % (ids[0], ids[-1], len(ids)))
     qs = model.objects.filter(id__in=ids)
     for item in qs:
-        model.index(item.fields(), bulk=True, id=item.id)
+        model.index(item.fields(), bulk=True, id_=item.id, es=es)
     es.flush_bulk(forced=True)
-
 
 @task
 def unindex_objects(model, ids, **kw):
@@ -37,4 +37,4 @@ def unindex_objects(model, ids, **kw):
         return
     for id in ids:
         log.info('Removing object [%s.%d] from search index.' % (model, id))
-        elasticutils.get_es().delete(model._get_index(), model._meta.db_table, id)
+        get_es().delete(model.get_index(), model._meta.db_table, id)
